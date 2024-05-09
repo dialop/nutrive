ce376151752fec1687ac314faa1c2f6eace11382
dialop <dianalop1998@gmail.com>
2024-05-08 22:32:07 -0400

    initial commit

--- nutrive/src/components/HelloWorld.vue
+++ nutrive/src/components/HelloWorld.vue
@@ -1,10 +1,16 @@
+<template>
+  <div class="hello">
+  
+  </div>
+</template>
+
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String,
-  },
-};
+    msg: String
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
